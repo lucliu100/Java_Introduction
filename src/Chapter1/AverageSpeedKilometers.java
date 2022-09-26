ca9cc5a338faf122eb469d245576cc608bad94ca
lucliu100 <112734443+lucliu100@users.noreply.github.com>
2022-09-25 22:27:02 -0700

    Finished AverageSpeedKilometers.java

---
+++ src/Chapter1/AverageSpeedKilometers.java
@@ -0,0 +1,8 @@
+package Chapter1;
+
+public class AverageSpeedKilometers {
+    public static void main(String[] args){
+        double time = (100*60) + 35;
+        System.out.println((24*1.6)/(time/3600));
+    }
+}
